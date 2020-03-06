e8a78e190b01d47510f860c1e04c95639918517f
Brianna Ondris <ondrisbb@vcu.edu>
2020-03-05 20:33:15 -0500

    loading gif and structure gallery page!

--- bris-website/src/app/preloading.directive.ts
+++ bris-website/src/app/preloading.directive.ts
@@ -6,10 +6,11 @@ import { Directive, ElementRef, AfterViewInit, Renderer2, OnInit, Input,AfterCon
 export class PreLoadingDirective implements OnInit {
   @Input('src') src;
 
-  constructor(private elemRef: ElementRef, private renderer: Renderer2) { }
+  constructor(private elemRef: ElementRef, private renderer: Renderer2){ }
   
   ngOnInit() {
-    this.renderer.setAttribute(this.elemRef.nativeElement, 'src', 'https://2.bp.blogspot.com/-xGlousScPfM/WEQB74UGXHI/AAAAAAAAAOo/Q3xV3xqOYdoIo492BtMCE0BC13-Q3ZXmgCLcB/s640/LOADING.gif');
+    // this.renderer.setAttribute(this.elemRef.nativeElement, 'src', 'https://2.bp.blogspot.com/-xGlousScPfM/WEQB74UGXHI/AAAAAAAAAOo/Q3xV3xqOYdoIo492BtMCE0BC13-Q3ZXmgCLcB/s640/LOADING.gif');
+    this.renderer.addClass(this.elemRef.nativeElement, 'gradient');
 
     const image = new Image();
     image.onload = this.imageLoaded.bind(this);
